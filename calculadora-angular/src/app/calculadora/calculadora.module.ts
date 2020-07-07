5cfd67185b483fc019ec51998e9d46b4592e8834
condor <e.hhdelemos@gmail.com>
2020-07-06 21:45:26 -0300

    Configura calculadora

---
+++ calculadora-angular/src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,20 @@
+import { CalculadoraService } from './calculadora.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+
+
+@NgModule({
+  declarations: [CalculadoraComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
+  ]
+})
+export class CalculadoraModule { }
